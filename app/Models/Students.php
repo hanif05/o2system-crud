e52e894102c4c787a93df5a287f55c94560321bf
hanif05 <muamarhanif05@gmail.com>
2019-10-10 11:12:19 +0700

    add model & controller students

---
+++ app/Models/Students.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by O2System Framework File Generator.
+ * DateTime: 10/10/2019 11:10
+ */
+
+// ------------------------------------------------------------------------
+
+namespace App\Models;
+
+// ------------------------------------------------------------------------
+
+use O2System\Framework\Models\Sql\Model;
+
+/**
+ * Class Students
+ *
+ * @package \App\Models
+ */
+class Students extends Model
+{
+    public $table = 'students';
+}
